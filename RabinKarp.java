4e0eb5b3ab2be5c42830c154e632505aa8248cc6
Daniel Harbor <harbord2@gmail.com>
2019-02-03 10:07:53 -0800

    RabinKarp implemenation using trivial hash function

--- RabinKarp.java
+++ RabinKarp.java
@@ -1,10 +1,13 @@
+// Uses rolling hash technique: compare key hash with hash of substrings
 public class RabinKarp {
     public static void main(String[] args) {
         System.out.println(contains("helloworld", "hello"));
     }
 
+    // simple character hash function using a prime (the larger prime,
+    // the better)
     static int getCharHash(char c) {
-        return (c * 421); // using a random prime here
+        return (c * 421);
     }
 
     static int getHash(String str) {
@@ -40,7 +43,8 @@ public class RabinKarp {
                 }
             }
             if (++lastCharIndex < str.length()) {
-                strHash = strHash - getCharHash(str.charAt(firstCharIndex++)) + getCharHash(str.charAt(lastCharIndex));
+                strHash = strHash - getCharHash(str.charAt(firstCharIndex++)) +
+                            getCharHash(str.charAt(lastCharIndex));
             }
         }
         return false;
