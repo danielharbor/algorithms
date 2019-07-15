4bb0d596c628a2b8d52aa2e9baee30ccd4aed9fa
Daniel Harbor <harbord2@gmail.com>
2019-07-14 18:26:52 -0700

    Check if graph input is a digraph while attempting the topological sort

--- src/com/topics/sort/TopologicalSort.java
+++ src/com/topics/sort/TopologicalSort.java
@@ -7,12 +7,19 @@ import com.topics.datastructures.AdjacencyListGraph;
 class TopologicalSort {
     public static void main(String[] args) {
         System.out.println("Topological Sort");
-        AdjacencyListGraph graph = new AdjacencyListGraph();
-        populateGraph(graph);
-        sortGraph(graph);
+        System.out.println("----------------");
+        AdjacencyListGraph graph = createGraph();
+        AdjacencyListGraph graphWithCycle = createGraphWithCycle();
+        try {
+            sortGraph(graph);
+            sortGraph(graphWithCycle);
+        } catch(Exception ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 
-    static void populateGraph(AdjacencyListGraph graph) {
+    static AdjacencyListGraph createGraph() {
+        AdjacencyListGraph graph = new AdjacencyListGraph();
         GraphNode nodeA = new GraphNode("A");
         GraphNode nodeB = new GraphNode("B");
         GraphNode nodeC = new GraphNode("C");
@@ -23,28 +30,52 @@ class TopologicalSort {
         graph.addEdge(nodeC, nodeD);
         graph.addEdge(nodeA, nodeC);
         System.out.println(graph);
+        return graph;
+    }
+
+    static AdjacencyListGraph createGraphWithCycle() {
+        AdjacencyListGraph graph = new AdjacencyListGraph();
+        GraphNode nodeA = new GraphNode("A");
+        GraphNode nodeB = new GraphNode("B");
+        GraphNode nodeC = new GraphNode("C");
+        graph.addEdge(nodeA, nodeB);
+        graph.addEdge(nodeB, nodeC);
+        graph.addEdge(nodeC, nodeA);
+        System.out.println(graph);
+        return graph;
     }
 
     static void sortGraph(AdjacencyListGraph adjlGraph) {
         Set<GraphNode> visited = new HashSet();
         Deque<GraphNode> stack = new ArrayDeque();
         Map<GraphNode, Set<GraphNode>> graph = adjlGraph.getGraph();
+        System.out.print("Output: ");
         for (GraphNode vertex : graph.keySet()) {
-            sortGraphHelper(graph, vertex, visited, stack);
+            Set<GraphNode> visitedCycleChecker = new HashSet();
+            sortGraphHelper(graph, vertex, visited, stack, visitedCycleChecker, 1);
         }
         System.out.println(stack);
     }
 
     static void sortGraphHelper(Map<GraphNode, Set<GraphNode>> graph, GraphNode vertex,
-                            Set<GraphNode> visited, Deque<GraphNode> stack) {
+                            Set<GraphNode> visited, Deque<GraphNode> stack,
+                            Set<GraphNode> visitedCycleChecker, int j) {
+        // check if graph has cycle by making sure a vertex isn't visited twice
+        if (visitedCycleChecker.contains(vertex)) {
+            throw new RuntimeException("Graph has a cycle at vertex " + vertex + "!");
+        }
+        visitedCycleChecker.add(vertex);
+
         if (!visited.contains(vertex)) {
             visited.add(vertex);
             // traverse all nodes connected to vertex
-            System.out.println(vertex);
             for (GraphNode node : graph.get(vertex)) {
-                sortGraphHelper(graph, node, visited, stack);
+                sortGraphHelper(graph, node, visited, stack, visitedCycleChecker, j+1);
             }
             stack.push(vertex);
         }
+
+        // important: remove vertex at the end if no cycle was detected
+        visitedCycleChecker.remove(vertex);
     }
 }
