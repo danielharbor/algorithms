150a88872481a69b6dfdf0d1f34a66788f5984e9
Daniel Harbor <harbord2@gmail.com>
2021-08-22 19:29:14 -0700

    Implement Pair with AbstractMap

---
+++ src/com/topics/datastructures/Pair.java
@@ -0,0 +1,18 @@
+package com.topics.datastructures;
+
+import java.util.AbstractMap;
+import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Pair {
+    static <T, U> Map.Entry<T, U> of(T first, U second) {
+        return new AbstractMap.SimpleEntry<>(first, second);
+    }
+
+    public static void main(String... args) {
+        Set<Map.Entry<String, Integer>> set = new HashSet<>();
+        set.add(Pair.of("abc", 1));
+        System.out.println(set);
+    }
+}
