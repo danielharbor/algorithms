2342e88bb74a5fe426940312651e98fb12c6463e
Daniel Harbor <harbord2@gmail.com>
2021-08-22 19:23:28 -0700

    Implement right to left fisher-yates array shuffle

--- src/com/topics/array/InPlaceShuffle.java
+++ src/com/topics/array/InPlaceShuffle.java
@@ -3,11 +3,18 @@ package com.topics.array;
 import java.util.*;
 import java.util.Arrays;
 
+// Fisher-Yates shuffle / Knuth Shuffle
 public class InPlaceShuffle {
     public static void main(String[] args) {
-        int arr[] = {1,2,3,4,5}, n = arr.length;
+        shuffleLeftToRight();
+        shuffleRightToLeft();
+    }
+
+    static void shuffleLeftToRight() {
+        System.out.println("left to right:");
         Random rand = new Random();
-        System.out.println("Original: " + Arrays.toString(arr).replace("], ", "]\n").replace("[[", "[").replace("]]", "]"));
+        int arr[] = {1,2,3,4,5}, n = arr.length;
+        System.out.println("Original: " + Arrays.toString(arr));
 
         for (int i = 0; i < n; ++i) {
             int randIdx = rand.nextInt(n - i) + i;
@@ -16,6 +23,22 @@ public class InPlaceShuffle {
             arr[randIdx] = temp;
         }
 
-        System.out.println("Shuffled: " + Arrays.toString(arr).replace("], ", "]\n").replace("[[", "[").replace("]]", "]"));
+        System.out.println("Shuffled: " + Arrays.toString(arr));
+    }
+
+    static void shuffleRightToLeft() {
+        System.out.println("right to left:");
+        Random rand = new Random();
+        int arr[] = {1,2,3,4,5}, n = arr.length;
+        System.out.println("Original: " + Arrays.toString(arr));
+
+        for (int i = n - 1; i >= 0; --i) {
+            int randIdx = rand.nextInt(i + 1);
+            int temp = arr[i];
+            arr[i] = arr[randIdx];
+            arr[randIdx] = temp;
+        }
+
+        System.out.println("Shuffled: " + Arrays.toString(arr));
     }
 }
