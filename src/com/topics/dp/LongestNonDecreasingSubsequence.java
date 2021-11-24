1ecbf3a2e3def26d2cb7ea0907eb37935fe45f80
Daniel Harbor <harbord2@gmail.com>
2021-11-24 12:35:48 -0800

    Improve variable names for clarity

--- src/com/topics/dp/LongestNonDecreasingSubsequence.java
+++ src/com/topics/dp/LongestNonDecreasingSubsequence.java
@@ -16,11 +16,11 @@ public class LongestNonDecreasingSubsequence {
         }
         int dp[] = new int[nums.length], longest = 1;
         java.util.Arrays.fill(dp, 1); // Each element is a non-decreasing subsequence of length 1
-        for (int j = 0; j < nums.length; ++j) {
-            for (int i = 0; i < j; ++i) {
-                if (nums[j] >= nums[i]) {
-                    dp[j] = Math.max(dp[j], dp[i] + 1);
-                    longest = Math.max(longest, dp[j]);
+        for (int fixed = 0; fixed < nums.length; ++fixed) {
+            for (int runner = 0; runner < fixed; ++runner) {
+                if (nums[fixed] >= nums[runner]) {
+                    dp[fixed] = Math.max(dp[fixed], dp[runner] + 1);
+                    longest = Math.max(longest, dp[fixed]);
                 }
             }
         }
