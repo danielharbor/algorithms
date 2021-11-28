cf1d5edbf14d2be2fd380dc6bdc71d8429850724
Daniel Harbor <harbord2@gmail.com>
2021-11-28 13:21:17 -0800

    Import Map.Entry to simplify Pair implementation

--- src/com/topics/datastructures/Pair.java
+++ src/com/topics/datastructures/Pair.java
@@ -1,7 +1,7 @@
 package com.topics.datastructures;
 
 import java.util.AbstractMap;
-import java.util.Map;
+import java.util.Map.Entry;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -9,12 +9,12 @@ import java.util.Set;
  * Reference: https://www.techiedelight.com/five-alternatives-pair-class-java/
  */
 public class Pair {
-    static <T, U> Map.Entry<T, U> of(T first, U second) {
+    static <T, U> Entry<T, U> of(T first, U second) {
         return new AbstractMap.SimpleEntry<>(first, second);
     }
 
     public static void main(String... args) {
-        Set<Map.Entry<String, Integer>> set = new HashSet<>();
+        Set<Entry<String, Integer>> set = new HashSet<>();
         set.add(Pair.of("abc", 1));
         System.out.println(set);
     }
